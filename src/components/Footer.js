4f995a26bfbff3df4fcea09febce880f1d40f14f
saharbenlalla <saharbenlalla212@gmail.com>
2023-11-12 00:23:39 +0100

    Mise en place du header et du footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,10 @@
+import styles from './Footer.module.scss';
+
 function Footer() {
   return (
-    <div className="b2">Footer</div>
+    <footer className={`${styles.footer} d-flex flex-row align-items-center justify-content-center p-20`}>
+      <p>Copyright ©2023 Recette Project By Sahar, Inc.</p>
+    </footer>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
+import styles from './Header.module.scss';
+import recette from '../assets/images/Logo_recettes_en_famille.png'
 function Header() {
   return (
-    <div className="b1">Header</div>
+    <header className={`${styles.header} d-flex flex-row align-items-center`}>
+      <i class="fa-solid fa-bars mr-15"></i>
+      <div className='flex-fill'>
+        <img src={recette} alt="logo" />
+      </div>
+      <ul>
+        <button className='btn btn-reverse-primary mr-5'><i class="fa-solid fa-basket-shopping mr-5"></i><span>Panier</span></button>
+        <button className='btn btn-primary mr-5'>Connexion</button>
+      </ul>
+    </header>
   )
 }
-
 export default Header
